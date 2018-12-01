30b99ddef5fe40eba39bd8562c0b848310c383f8
chikara <chikara.f.yoshida@gmail.com>
2018-12-01 16:12:06 +0900

    emmetすげえ render_template

--- 02.py
+++ 02.py
@@ -1,14 +1,18 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("omikuji.html")
 
-@app.route("/user/<name>")
-def hello(name):
-    return render_template("hello.html", name=name)
+
+@app.route("/omikuji")
+def hello():
+    omikuji_list = ["大吉", '吉', '凶']
+    return random.choice(omikuji_list)
 
 
 if __name__ == '__main__':
