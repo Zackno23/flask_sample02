21f15b9862b6586b7aa3690480eb24d99f3e0734
hikanaka <roundupusa02@gmail.com>
2018-12-01 16:25:45 +0900

    omikuji app

--- 02.py
+++ 02.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("omikuji.html")
@@ -15,6 +16,16 @@ def hello():
     return random.choice(omikuji_list)
 
 
+
+
+@app.route("/dice")
+def dice_shuffle():
+    dice_list = range(0, 5)
+    return random.choice(dice_list)
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
